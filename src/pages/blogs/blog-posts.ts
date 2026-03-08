b0d7f8fccfa11318f4d50d8049d4c7e0626a0f28
mitch8020 <jpmitra.swe@gmail.com>
2026-03-08 12:48:47 -0500

    Add a raw thoughts blog entry

--- src/pages/blogs/blog-posts.ts
+++ src/pages/blogs/blog-posts.ts
@@ -1,7 +1,11 @@
 import { futuresWorthPlayingForPost } from './articles/FuturesWorthPlayingForArticle'
+import { rawThoughtsPost } from './articles/FuturesWorthPlayingForArticleRaw'
 import type { BlogPost } from './blog-types'
 
-export const blogPosts: BlogPost[] = [futuresWorthPlayingForPost]
+export const blogPosts: BlogPost[] = [
+  rawThoughtsPost,
+  futuresWorthPlayingForPost,
+]
 
 export function getBlogPostBySlug(slug: string) {
   return blogPosts.find((post) => post.slug === slug)
